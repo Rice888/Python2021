bb6558d1a1b758a8522c26c10f593af6996026e0
Rice <rice88@outlook.com>
2020-06-17 17:06:04 +0800

    0617

---
+++ 02-py-def/def02.py
@@ -0,0 +1,10 @@
+#encoding:utf-8
+'''
+@Author: your name
+@Date: 2020-06-17 17:04:31
+@LastEditTime: 2020-06-17 17:05:47
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /RiceProgram/python3/02-py-def/def02.py
+'''
+#def02.py
