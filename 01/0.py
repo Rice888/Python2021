e66918f7721c81c284d4fc57ae8f6a9bca5b1fff
Rice <rice88@outlook.com>
2021-04-23 11:00:57 +0800

    first game

---
+++ 01/0.py
@@ -0,0 +1,41 @@
+'''
+Author: your name
+Date: 2021-04-23 10:52:18
+LastEditTime: 2021-04-23 10:56:37
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /python2021/01/0.py
+'''
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 640, 480
+dx = 1
+dy = 1
+x= 163
+y = 120
+black = (0,0,0)
+white = (255,255,255)
+
+screen = pygame.display.set_mode(size)
+
+while 1:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    x += dx
+    y += dy
+
+    if x < 0 or x > width:   
+        dx = -dx
+
+    if y < 0 or y > height:
+        dy = -dy
+
+    screen.fill(black)
+
+    pygame.draw.circle(screen, white, (x,y), 8)
+
+    pygame.display.flip()
